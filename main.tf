8542fe2a77f758ea7e22b50a3af985e8995ac6d2
jorge-nclouds <jorge@nclouds.com>
2025-07-23 11:32:00 -0600

    2 buckets

--- main.tf
+++ main.tf
@@ -6,3 +6,12 @@ resource "aws_s3_bucket" "example" {
     Project = "terraform-plan"
   }
 }
+
+resource "aws_s3_bucket" "example" {
+  bucket = "2-poc-s3-bucket-afg-tf-plan"
+
+  tags = {
+    Environment = "Dev"
+    Project = "terraform-plan"
+  }
+}
